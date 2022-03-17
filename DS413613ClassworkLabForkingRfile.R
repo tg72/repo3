b329fce44424e9bac3ce6dd34e5338b687dd1322
tg72 <78769244+tg72@users.noreply.github.com>
2022-03-16 23:11:33 -0400

    update classwork file

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,9 +2,9 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is Dennis P.
+# My name is Taylor Gordon.
 #The data source is the mpg data table
-# This is my coding that generates Boxplots for transmission types
+# This is my code that changes the boxplots to horizontal with coord flip. 
 ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
